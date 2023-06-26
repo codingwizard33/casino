e3ba9ef54791dd78dcefb9c26eba2e67126c1390
codingwizard33 <codingwizard33@gmail.com>
2023-06-26 12:25:49 +0400

    change user role permissions

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,10 +4,12 @@ import { verifyMiddleware } from '../middlewares/verifyMiddleware.js';
 import { createSuperAgent } from '../controllers/admin/createSuperAgentController.js';
 import { adminMiddleware } from '../middlewares/adminMiddleware.js';
 import { getSuperAgents } from '../controllers/getSuperAgentsController.js';
+import { superAgentMiddleware } from '../middlewares/superAgentMiddleware.js';
+import { agentMiddleware } from '../middlewares/agentMiddleware.js';
 
 const router = express.Router();
 
-router.post('/create-super-agent', authMiddleware, verifyMiddleware, adminMiddleware, createSuperAgent);
-router.post('/get-super-agents', authMiddleware, verifyMiddleware, adminMiddleware, getSuperAgents);
+router.post('/create-super-agent', authMiddleware, verifyMiddleware, adminMiddleware, superAgentMiddleware, agentMiddleware, createSuperAgent);
+router.post('/get-super-agents', authMiddleware, verifyMiddleware, adminMiddleware, superAgentMiddleware, agentMiddleware, getSuperAgents);
 
 export default router;
--- src/routes/superAgent.js
+++ src/routes/superAgent.js
@@ -4,10 +4,11 @@ import { verifyMiddleware } from '../middlewares/verifyMiddleware.js';
 import { createAgent } from '../controllers/super-agent/createAgentController.js';
 import { superAgentMiddleware } from '../middlewares/superAgentMiddleware.js';
 import { getAgents } from '../controllers/getAgentsController.js';
+import { agentMiddleware } from '../middlewares/agentMiddleware.js';
 
 const router = express.Router();
 
-router.post('/create-agent', authMiddleware, verifyMiddleware, superAgentMiddleware, createAgent);
+router.post('/create-agent', authMiddleware, verifyMiddleware, superAgentMiddleware, agentMiddleware, createAgent);
 router.post('/get-agents', authMiddleware, verifyMiddleware, getAgents);
 
 export default router;
